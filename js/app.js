ab73732215b0ac32d380d9f95253cfa57a94f912
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-11-30 22:38:08 +0100

    add md parser, change appname

--- js/app.js
+++ js/app.js
@@ -2,15 +2,16 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('devXiveDevDocs', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'devXiveDevDocs.filters',
+  'devXiveDevDocs.services',
+  'devXiveDevDocs.directives',
+  'devXiveDevDocs.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/docPage.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/docPage.html', controller: 'MyCtrl2'});
+
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
