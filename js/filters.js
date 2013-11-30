065a6cb830457f4d55ac6e64efce098b4c1a50d2
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-11-30 22:45:17 +0100

    add marked to filter

--- js/filters.js
+++ js/filters.js
@@ -6,5 +6,14 @@ angular.module('devXiveDevDocs.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+    };
+  }]).
+  filter('marked', function() {
+    return function(input) {
+      if (typeof(input) != 'undefined') {
+        var out = marked(input);
+
+        return out;
+      }
+    };
+  });
