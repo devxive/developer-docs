8581b524956b6cd16ea632fcfcd0f26394d1b4db
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-12-01 06:11:47 +0100

    update urls

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ angular.module('devXiveDevDocs', [
     $routeProvider.when('/:lang/:part1/:part2/:part3', {templateUrl: 'partials/docPage.html', controller: DocController});
     $routeProvider.when('/:lang/:part1/:part2/:part3/:part4', {templateUrl: 'partials/docPage.html', controller: DocController});
 
-    $routeProvider.otherwise({redirectTo: '/en/home.md'});
+    $routeProvider.otherwise({redirectTo: 'en/home.md'});
   }]).
   config(function($sceDelegateProvider) {
     $sceDelegateProvider.resourceUrlWhitelist([
